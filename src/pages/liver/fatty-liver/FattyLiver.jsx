f37558fcea439d441fa2de9b77ce1d0236bbcc43
Hassen David Ortiz <hassen.ortiz@correounivalle.edu.co>
2025-07-07 22:17:09 -0500

    Actualización CSS

--- src/pages/liver/fatty-liver/FattyLiver.jsx
+++ src/pages/liver/fatty-liver/FattyLiver.jsx
@@ -1,8 +1,11 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import React, { useState } from 'react'
 import LiverModel from './LiverModel'
 import './FattyLiver.css'
 =======
+=======
+>>>>>>> Stashed changes
 import React, { useState, useEffect, useRef } from "react"
 import { Link } from "react-router-dom"
 import LiverModel from "../LiverModel"
@@ -10,6 +13,9 @@ import { IoIosHelpCircleOutline } from "react-icons/io"
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md"
 import "./FattyLiver.css"
 import "../controls.css"
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
 export default function FattyLiver() {
